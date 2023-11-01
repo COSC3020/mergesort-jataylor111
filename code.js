8c1ece6ce950941d1e87ea24a12a59be98889629
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2023-11-01 14:53:44 -0600

    Update code.js

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ function mergesort(arr) {
   for (var curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) {
       for (var left_start = 0; left_start < n; left_start += 2 * curr_size) {
           var mid = Math.floor(left_start + curr_size - 1, n - 1);
-          var right_end = Math.floor(left_start + 2 * curr_size - 1, n - 1);
+          var right_end = Math.min(left_start + 2 * curr_size - 1, n - 1);
           var start = left_start;
           var start2 = mid + 1;
 
