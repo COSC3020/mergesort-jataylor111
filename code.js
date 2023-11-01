b4b7ebc8d4e9ded75df6a77e0ece7dd66f0899ed
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2023-11-01 14:34:28 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,37 @@
 function mergesort(array) {
+    var n = arr.length;
+
+  for (var curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) {
+      for (var left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
+          var mid = Math.floor(left_start + curr_size - 1, n - 1);
+          var right_end = Math.floor(left_start + 2 * curr_size - 1, n - 1);
+          var start = left_start;
+          var start2 = mid + 1;
+
+          if (arr[mid] <= arr[start2]) {
+              continue;
+          }
+
+          while (start <= mid && start2 <= right_end) {
+              if (arr[start] <= arr[start2]) {
+                  start++;
+              } else {
+                  var value = arr[start2];
+                  var index = start2;
+
+                  while (index != start) {
+                      arr[index] = arr[index - 1];
+                      index--;
+                  }
+                  arr[start] = value;
+
+                  start++;
+                  mid++;
+                  start2++;
+              }
+          }
+      }
+  }
+  return arr;
     return array;
 }
